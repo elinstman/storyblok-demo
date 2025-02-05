31bfa281e34d73a9520562246d2a52d123099cb7
Elin Östman <elin.ostman@hotmail.com>
2025-02-05 12:13:22 +0100

    new route config

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,6 @@
-import { type RouteConfig } from "@react-router/dev/routes";
-import { flatRoutes } from "@react-router/fs-routes";
+import { type RouteConfig, route } from "@react-router/dev/routes";
 
-export default flatRoutes() satisfies RouteConfig;
-
-
-// export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  route("/", "./routes/$slug.tsx" ),
+  route("/*", "./routes/test.tsx"),
+] satisfies RouteConfig;
